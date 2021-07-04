483a68ad8bb23adb559feadab326fce273e32eea
David <kudavidss@gmail.com>
2021-07-04 14:40:38 +0300

    added navbar transition

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
   <nav class="nav">
-    <input type="checkbox" id="menu-toggle" v-model="navOpened">
+    <input type="checkbox" id="menu-toggle" v-model="navOpened" />
     <label class="menu-btn" for="menu-toggle">
       <span></span>
     </label>
     <div class="nav-logo">
       <router-link to="/">
-        <img :src="require('@/assets/images/logo.png')" alt="">
+        <img :src="require('@/assets/images/logo.png')" alt="" />
         <div class="nav-brand">{{ navText }}</div>
       </router-link>
     </div>
@@ -25,7 +25,7 @@ export default {
     return {
       navOpened: false,
       navText: 'AUZM STUDIO'
-    }
+    };
   },
   created() {
     window.addEventListener('resize', this.refreshNavSize);
@@ -46,160 +46,165 @@ export default {
       }, 100);
     }
   }
-}
+};
 </script>
 
 <style>
-  .nav {
-    display: flex;
-    position: fixed;
-    align-items: center;
-    width: 100%;
-    height: 75px;
-    z-index: 1;
-    border-bottom: 1px solid #ffffff1c;
-    backdrop-filter: blur(20px);
-    -webkit-backdrop-filter: blur(20px);
-  }
-  .nav-logo, .nav-links {
-    display: flex;
-    flex-basis: 33%;
-  }
-  .nav-logo {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #fff;
-    font-weight: bold;
-    height: 100%;
-  }
-  .nav-logo img {
-    width: 55px;
-    height: auto;
-    cursor: pointer;
-  }
-  .nav-brand {
-    margin-left: 15px;
-    font-size: 18px;
+.nav {
+  display: flex;
+  position: fixed;
+  align-items: center;
+  width: 100%;
+  height: 75px;
+  z-index: 1;
+  border-bottom: 1px solid #ffffff1c;
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
+}
+.nav-logo,
+.nav-links {
+  display: flex;
+  flex-basis: 33%;
+}
+.nav-logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  font-weight: bold;
+  height: 100%;
+}
+.nav-logo img {
+  width: 55px;
+  height: auto;
+  cursor: pointer;
+}
+.nav-brand {
+  margin-left: 15px;
+  font-size: 18px;
+}
+.nav-links {
+  justify-content: space-between;
+}
+.nav a {
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 100%;
+  line-height: 46px;
+  padding: 0 5px;
+  color: #fff;
+  font-size: 17px;
+  font-weight: bold;
+  transition: background 0.3s, color 0.3s;
+}
+.nav .nav-links a::before {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: #fff;
+  visibility: hidden;
+  transform: scaleX(0);
+  transition: all 0.3s ease-in-out 0s;
+}
+.nav .nav-links a:hover::before,
+.nav .nav-links a.active::before {
+  visibility: visible;
+  transform: scaleX(1);
+}
+#menu-toggle {
+  height: 50px;
+}
+#menu-toggle:checked ~ .menu-btn > span {
+  transform: rotate(45deg);
+}
+#menu-toggle:checked ~ .menu-btn > span::before {
+  top: 0;
+  transform: rotate(0);
+}
+#menu-toggle:checked ~ .menu-btn > span::after {
+  top: 0;
+  transform: rotate(90deg);
+}
+#menu-toggle:checked ~ .menu__box {
+  visibility: visible;
+  left: 0;
+}
+.menu-btn {
+  align-items: flex-start;
+  position: fixed;
+  top: 38px;
+  right: 30px;
+  width: 26px;
+  cursor: pointer;
+  z-index: 1;
+}
+.menu-btn > span,
+.menu-btn > span::before,
+.menu-btn > span::after {
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  background-color: #ffffff;
+  transition-duration: 0.25s;
+}
+.menu-btn > span::before {
+  content: '';
+  top: -8px;
+}
+.menu-btn > span::after {
+  content: '';
+  top: 8px;
+}
+@media (min-width: 768px) {
+  #menu-toggle,
+  .menu-btn {
+    display: none;
   }
   .nav-links {
-    justify-content: space-between;
+    height: 100%;
+  }
+}
+@media (max-width: 768px) {
+  .nav {
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    height: auto;
+  }
+  .nav-links {
+    max-height: 0;
+    flex-direction: column;
+    width: 100%;
+    overflow: hidden;
+    transition: max-height 0.3s ease;
+  }
+  .nav-links.expanded {
+    max-height: 135px;
+  }
+  .nav-logo {
+    margin: 10px 20px;
   }
   .nav a {
-    position: relative;
+    margin: 0 0%;
+    border: none;
     display: flex;
-    align-items: center;
-    height: 100%;
-    line-height: 46px;
-    padding: 0 5px;
-    color: #fff;
-    font-size: 17px;
-    font-weight: bold;
-    transition: background 0.3s, color 0.3s;
+    justify-content: center;
   }
-  .nav .nav-links a::before {
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    background-color: #fff;
+  .nav .nav-links a:hover::before,
+  .nav .nav-links a.active::before {
     visibility: hidden;
-    transform: scaleX(0);
-    transition: all 0.3s ease-in-out 0s;
-  }
-  .nav .nav-links a:hover::before, .nav .nav-links a.active::before {
-    visibility: visible;
-    transform: scaleX(1);
-  }
-  #menu-toggle {
-    height: 50px;
-  }
-  #menu-toggle:checked ~ .menu-btn > span {
-    transform: rotate(45deg);
-  }
-  #menu-toggle:checked ~ .menu-btn > span::before {
-    top: 0;
-    transform: rotate(0);
-  }
-  #menu-toggle:checked ~ .menu-btn > span::after {
-    top: 0;
-    transform: rotate(90deg);
-  }
-  #menu-toggle:checked ~ .menu__box {
-    visibility: visible;
-    left: 0;
   }
   .menu-btn {
-    align-items: flex-start;
-    position: fixed;
-    top: 38px;
-    right: 30px;
-    width: 26px;
-    cursor: pointer;
-    z-index: 1;
+    display: flex;
   }
-  .menu-btn > span,
-  .menu-btn > span::before,
-  .menu-btn > span::after {
-    display: block;
-    position: absolute;
-    width: 100%;
-    height: 2px;
-    background-color: #ffffff;
-    transition-duration: .25s;
+  #menu-toggle {
+    display: none;
+    visibility: hidden;
   }
-  .menu-btn > span::before {
-    content: '';
-    top: -8px;
-  }
-  .menu-btn > span::after {
-    content: '';
-    top: 8px;
-  }
-  @media (min-width: 768px) {
-    #menu-toggle, .menu-btn {
-      display: none;
-    }
-    .nav-links {
-      height: 100%;
-    }
-  }
-  @media (max-width: 768px) {
-    .nav {
-      flex-direction: column;
-      justify-content: flex-start;
-      align-items: flex-start;
-      height: auto;
-    }
-    .nav-links {
-      display: none;
-      flex-direction: column;
-      width: 100%;
-      overflow: hidden;
-    }
-    .nav-links.expanded {
-      display: block;
-    }
-    .nav-logo {
-      margin: 10px 20px;
-    }
-    .nav a {
-      margin: 0 0%;
-      border: none;
-      display: flex;
-      justify-content: center;
-    }
-    .nav .nav-links a:hover::before, .nav .nav-links a.active::before {
-      visibility: hidden;
-    }
-    .menu-btn {
-      display: flex;
-    }
-    #menu-toggle {
-      display: none;
-      visibility: hidden;
-    }
-  }
+}
 </style>
--- src/views/Members.vue
+++ src/views/Members.vue
@@ -1,170 +1,172 @@
 <template>
-	<div class="main">
-		<h1>{{ title }}</h1>
-		<div class="cards">
-			<div v-for="member in members" :key="member.name" class="card">
-				<img :src="member.avatar" :alt="member.name">
-				<div class="card-info">
-					<div class="name">{{ member.name }}</div>
-					<div class="role">{{ member.role }}</div>
-					<div class="social">
-						<a v-for="item in member.social" :key="item.name" :href="item.link" target="_blank" rel="noreferrer"><i :class="`fab fa-${item.name}`"></i></a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="main">
+    <h1>{{ title }}</h1>
+    <div class="cards">
+      <div v-for="member in members" :key="member.name" class="card">
+        <img :src="member.avatar" :alt="member.name" />
+        <div class="card-info">
+          <div class="name">{{ member.name }}</div>
+          <div class="role">{{ member.role }}</div>
+          <div class="social">
+            <a v-for="item in member.social" :key="item.name" :href="item.link" target="_blank" rel="noreferrer"
+              ><i :class="`fab fa-${item.name}`"></i
+            ></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-	export default {
-		name: 'members',
-		data () {
-			return {
-				title: 'Members',
-				members: [
-					{
-						name: 'Sagokko',
-						avatar: require('../assets/images/members/member-1.png'),
-						role: 'Content Maker',
-						social: [
-							{
-								name: 'instagram',
-								link: 'https://www.instagram.com/sagokko'
-							},
-							{
-								name: 'youtube',
-								link: 'https://www.youtube.com/c/Sagokko'
-							},
-						]
-					},
-					{
-						name: 'KiRpIcH',
-						avatar: require('../assets/images/members/member-2.png'),
-						role: 'Game Developer',
-						social: [
-							{
-								name: 'instagram',
-								link: 'https://instagram.com/auzmarts'
-							},
-							{
-								name: 'youtube',
-								link: 'https://www.youtube.com/channel/UCl9cd4V7o1g-9I9MQOwxy0g'
-							},
-						]
-					},
-					{
-						name: 'FLX',
-						avatar: require('../assets/images/members/member-3.png'),
-						role: 'Web Developer',
-						social: [
-							{
-								name: 'twitter',
-								link: 'https://twitter.com/flx_emt'
-							},
-							{
-								name: 'github',
-								link: 'https://github.com/flxemt'
-							},
-						]
-					},
-					{
-						name: 'Banditman',
-						avatar: require('../assets/images/members/member-4.png'),
-						role: 'Game Developer',
-						social: [
-							{
-								name: 'instagram',
-								link: 'https://www.instagram.com/lenin_grib1870'
-							},
-							{
-								name: 'youtube',
-								link: 'https://www.youtube.com/channel/UChdA0MA3LRYDx12-qKvD6tg'
-							},
-						]
-					}
-				]
-			}
-		}
-	}
+export default {
+  name: 'members',
+  data() {
+    return {
+      title: 'Members',
+      members: [
+        {
+          name: 'Sagokko',
+          avatar: require('../assets/images/members/member-1.png'),
+          role: 'Content Maker',
+          social: [
+            {
+              name: 'instagram',
+              link: 'https://www.instagram.com/sagokko'
+            },
+            {
+              name: 'youtube',
+              link: 'https://www.youtube.com/c/Sagokko'
+            }
+          ]
+        },
+        {
+          name: 'KiRpIcH',
+          avatar: require('../assets/images/members/member-2.png'),
+          role: 'Game Developer',
+          social: [
+            {
+              name: 'instagram',
+              link: 'https://instagram.com/auzmarts'
+            },
+            {
+              name: 'youtube',
+              link: 'https://www.youtube.com/channel/UCl9cd4V7o1g-9I9MQOwxy0g'
+            }
+          ]
+        },
+        {
+          name: 'flxemt',
+          avatar: require('../assets/images/members/member-3.png'),
+          role: 'Web Developer',
+          social: [
+            {
+              name: 'twitter',
+              link: 'https://twitter.com/flxemt'
+            },
+            {
+              name: 'github',
+              link: 'https://github.com/flxemt'
+            }
+          ]
+        },
+        {
+          name: 'Banditman',
+          avatar: require('../assets/images/members/member-4.png'),
+          role: 'Game Developer',
+          social: [
+            {
+              name: 'instagram',
+              link: 'https://www.instagram.com/lenin_grib1870'
+            },
+            {
+              name: 'youtube',
+              link: 'https://www.youtube.com/channel/UChdA0MA3LRYDx12-qKvD6tg'
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped>
-	.main {
-		color: #fff;
-		font-family: 'Poppins', sans-serif;
-		padding-top: 20vh;
-	}
-	.cards {
-		display: flex;
-    max-width: 1000px;
-		margin: 0 auto;
-		margin-top: 3vh;
-		justify-content: space-between;
-		flex-wrap: wrap;
-		align-content: space-between;
-    height: 50vh;
-	}
-	.card {
-		background: rgba(17, 17, 17, .4);
-		backdrop-filter: blur(10px);
-		box-sizing: border-box;
-		color: #fff;
-		border-radius: 10px;
-		height: 180px;
-    width: 360px;
-		padding: 40px;
-    display: flex;
-    justify-content: space-between;
-		align-items: center;
-		margin-bottom: 10px;
-	}
-	.card img {
-		width: 110px;
-	}
-	.card-info {
-		width: 45%;
-    text-align: center;
-		font-weight: bold;
-	}
-	.card-info .name {
-		font-family: "Poppins", sans-serif;
-    font-size: 20px;
-	}
-	.card-info .role {
-		font-size: 14px;
-    font-family: 'Roboto', sans-serif;
-    color: #DADADA;
-	}
-	.card-info .social {
-		margin-top: 20px;
-		display: flex;
-		justify-content: center;
-	}
-	.card-info .social a:not(:last-child) {
-		margin-right: 20px;
-	}
-	.card-info .social i {
-		font-size: 24px;
-		transition: color 0.3s;
-	}
-	.card-info .social i:hover {
-		color: rgba(255, 255, 255, 0.7);
-	}
-	@media screen and (max-width: 768px) {
-		.cards {
-			flex-direction: column;
-			align-items: center;
-			padding: 0 5px;
-			height: auto;
-			align-content: center;
-		}
-		.card {
-			width: 100%;
-			max-width: 360px;
-			transform: scale(0.9);
-		}
-		.main {
-			overflow-y: auto;
-		}
-	}
+.main {
+  color: #fff;
+  font-family: 'Poppins', sans-serif;
+  padding-top: 20vh;
+}
+.cards {
+  display: flex;
+  max-width: 1000px;
+  margin: 0 auto;
+  margin-top: 3vh;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  align-content: space-between;
+  height: 50vh;
+}
+.card {
+  background: rgba(17, 17, 17, 0.4);
+  backdrop-filter: blur(10px);
+  box-sizing: border-box;
+  color: #fff;
+  border-radius: 10px;
+  height: 180px;
+  width: 360px;
+  padding: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+}
+.card img {
+  width: 110px;
+}
+.card-info {
+  width: 45%;
+  text-align: center;
+  font-weight: bold;
+}
+.card-info .name {
+  font-family: 'Poppins', sans-serif;
+  font-size: 20px;
+}
+.card-info .role {
+  font-size: 14px;
+  font-family: 'Roboto', sans-serif;
+  color: #dadada;
+}
+.card-info .social {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+}
+.card-info .social a:not(:last-child) {
+  margin-right: 20px;
+}
+.card-info .social i {
+  font-size: 24px;
+  transition: color 0.3s;
+}
+.card-info .social i:hover {
+  color: rgba(255, 255, 255, 0.7);
+}
+@media screen and (max-width: 768px) {
+  .cards {
+    flex-direction: column;
+    align-items: center;
+    padding: 0 5px;
+    height: auto;
+    align-content: center;
+  }
+  .card {
+    width: 100%;
+    max-width: 360px;
+    transform: scale(0.9);
+  }
+  .main {
+    overflow-y: auto;
+  }
+}
 </style>
